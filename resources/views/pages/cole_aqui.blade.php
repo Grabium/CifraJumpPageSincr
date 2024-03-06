2cd0eba111f2ea6d620da5696a4141bed4c90ada
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2024-03-06 19:54:44 +0000

    #2 - Foi adicionado um arquivo CSS para finalidade de testes que ainda precisa de muito polimento.

--- resources/views/pages/cole_aqui.blade.php
+++ resources/views/pages/cole_aqui.blade.php
@@ -19,13 +19,19 @@
 
         <input name="fator" id="fator" value="{{ $fator }}" /> 
         <button id="diminuir" class="btn btn-danger aument_dimin" onclick="intervalo('-')">-</button>
-        <!--<label id="lab_fator">0</label>-->
+        <label id="lab_fator">0</label>
         <button id="aumentar" class="btn btn-danger aument_dimin" onclick="intervalo('+')" >+</button>
 
       </div>
     </div>
 
-
+    <div id="div_alterar">
+      <div id="div_filha_alterar">
+        <button id="but_alterar" class="btn btn-danger" >Alterar</button>
+    
+      </div>
+    </div>
+    
     <div class="div_coleabaixo">Insira o texto no campo abaixo</div>
     <div class="div_coleabaixo">Ajuste a tonalidade desejada.</div>
 
